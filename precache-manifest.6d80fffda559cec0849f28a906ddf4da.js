a45e153178763ff095ab97eefc79394bdc145645
Anastasiia Bortnichuk <feilanheid@gmail.com>
2020-07-30 22:20:17 +0300

    Updates

---
+++ precache-manifest.6d80fffda559cec0849f28a906ddf4da.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bbf1c15cdaa9e17ba0026a84fe902e6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "71170d6ec395cd0c7b36",
+    "url": "./static/css/main.13026b85.chunk.css"
+  },
+  {
+    "revision": "2dccce6a023ac7bdcd3d",
+    "url": "./static/js/2.75343c50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.75343c50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71170d6ec395cd0c7b36",
+    "url": "./static/js/main.10ce212b.chunk.js"
+  },
+  {
+    "revision": "1610068e25a4810046d0",
+    "url": "./static/js/runtime-main.e1f50cee.js"
+  },
+  {
+    "revision": "72b4e57da39ad335c2539f258c5c5306",
+    "url": "./static/media/ArrowLeft.72b4e57d.svg"
+  },
+  {
+    "revision": "c04f58ba855a95a0fea25837001a3e4e",
+    "url": "./static/media/ArrowRight.c04f58ba.svg"
+  },
+  {
+    "revision": "5e26ee54ae8e8fa9a1e644fd6814a16a",
+    "url": "./static/media/ArrowUp.5e26ee54.svg"
+  },
+  {
+    "revision": "8d884803928628aa7b15861d143e5ee0",
+    "url": "./static/media/FilledHeartLike.8d884803.svg"
+  },
+  {
+    "revision": "35e96039ec2cf466f032cef3678f8102",
+    "url": "./static/media/HeartLike.35e96039.svg"
+  },
+  {
+    "revision": "482e37260cd688576287f1976e958e0b",
+    "url": "./static/media/Mont-Bold.482e3726.otf"
+  },
+  {
+    "revision": "f0f845588eef439ac6ecc9b6ab42191c",
+    "url": "./static/media/Mont-Regular.f0f84558.otf"
+  },
+  {
+    "revision": "d41a520b1802421f24e21279736e1871",
+    "url": "./static/media/Mont-SemiBold.d41a520b.otf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.f96163bf66b4bc18893bcedc683b7c29.js"
+  "./precache-manifest.6d80fffda559cec0849f28a906ddf4da.js"
 );
 
 self.addEventListener('message', (event) => {
